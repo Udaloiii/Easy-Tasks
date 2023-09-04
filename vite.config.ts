678f71801c737ba4ee5b67f10fb6c32771a66dbb
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-04 14:04:25 +0300

    changed localhost port from 5173 to 3001 and added so that the project itself opens by clicking on the dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
 import * as path from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
-    },
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    host: 'localhost',
+    port: 3001,
+    open: true,
+  }, // поменял порт из-за cors-ошибки ( запросы на сервер social-network разрешено только с localhost:3000, localhost:3001, localhost:9009)
 })
