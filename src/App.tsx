909fcf66c64de3ddd5d6e2c022bd450727fbc5f6
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-05 11:38:52 +0300

    changed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { useEffect } from 'react'
-
-import { todolistApi } from '@/common/api/common-api.ts'
-
 export function App() {
-  useEffect(() => {
-    todolistApi.getTodolists().then(res => console.log(res.data.map(el => el.title)))
-  }, [])
-
   return <div>Hello</div>
 }
