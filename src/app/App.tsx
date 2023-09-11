3d323f6b66ddd16e19386f7d7c092a14d25f32e7
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-11 14:13:03 +0300

    added style for App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,16 @@
+import { Todolist } from '../ui/todolist/todolist.tsx'
+
+import s from './app.module.scss'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div className={s.mainBlock}>
+      <Todolist />
+      <Todolist />
+      <Todolist />
+      <Todolist />
+      <Todolist />
+      <Todolist />
+    </div>
+  )
 }
