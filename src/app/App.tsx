b4ddeb4cec27e6b69c23f93b40d2275f1832b4bd
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-12 22:26:45 +0300

    changed project (added imports, remove 'base url' in tsconfig.json)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
-import { Todolist } from '../ui/todolist/todolist.tsx'
-
 import s from './app.module.scss'
+
+import { ItemAddingForm } from '@/common'
+import { Todolist } from '@/ui/todolist/todolist.tsx'
 
 export function App() {
   return (
     <div className={s.mainBlock}>
+      <ItemAddingForm />
       <Todolist />
       <Todolist />
       <Todolist />
