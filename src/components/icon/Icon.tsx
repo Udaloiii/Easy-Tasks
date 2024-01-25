6a3abc9b0fb648260c3f3380d389edf804e246c1
Udaloiii <udaloiiisohappy@gmail.com>
2024-01-25 17:11:00 +0300

    created universal Icon.tsx

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+
+import icon from '../../assets/icons/svgSprite.svg'
+
+type IconPropsType = {
+  height?: string
+  iconId: string
+  viewBox?: string
+  width?: string
+}
+export const Icon: FC<IconPropsType> = ({ height, iconId, viewBox, width }: IconPropsType) => {
+  return (
+    <svg
+      height={height || '24'}
+      viewBox={viewBox || '0 0 24 24'}
+      width={width || '24'}
+      xmlns={'http://www.w3.org/2000/svg'}
+    >
+      <use xlinkHref={`${icon}#${iconId}`}></use>
+    </svg>
+  )
+}
