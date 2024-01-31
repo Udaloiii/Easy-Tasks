ae4c0c51e323ec85c0661ceefde6b0455a29ef7e
Udaloiii <udaloiiisohappy@gmail.com>
2024-01-31 20:07:07 +0300

    added props; changed style

--- src/layout/todolist/buttonFilter/ButtonFilter.tsx
+++ src/layout/todolist/buttonFilter/ButtonFilter.tsx
@@ -15,18 +15,19 @@ export const ButtonFilter: FC<ButtonFilterPropsType> = ({
   title,
 }: ButtonFilterPropsType) => {
   return (
-    <StyleButton active={filter === title} onClick={() => onClick?.(title)}>
+    <StyleButton active={filter === title} onClick={() => onClick?.(title)} title={title}>
       <span>{title}</span>
     </StyleButton>
   )
 }
 
-const StyleButton = styled.button<{ active: boolean }>`
+const StyleButton = styled.button<{ active: boolean; title?: FilterValuesType }>`
   font-size: 17px;
   font-weight: bold;
   border: none;
   border-radius: 14px;
-  background: #000000;
+  //background: #000000;
+  background: ${props => (props.active ? 'transparent' : '#000000')};
   padding: 0;
 
   & span {
@@ -35,14 +36,18 @@ const StyleButton = styled.button<{ active: boolean }>`
     align-items: center;
     width: 110px;
     height: 40px;
+    background: rgba(65, 105, 225, 0.5);
     box-sizing: border-box;
-    border: ${props => (props.active ? 'none' : '2px solid #000000')};
+    border: ${props => (props.active ? '2px solid royalblue' : '2px solid #000000')};
     border-radius: 14px;
-    background: ${props => (props.active ? 'royalblue' : '#e8e8e8')};
-    color: ${props => (props.active ? 'whitesmoke' : '#000000')};
+    //background: ${props => (props.active ? '#e8e8e8' : '#e8e8e8')};
+    background: ${props => (props.active ? ' rgba(23, 23, 23, 0.5)' : '#e8e8e8')};
+    color: ${props => (props.active ? 'royalblue' : '#000000')};
     transform: ${props => (props.active ? 'translateY(0)' : 'translateY(-0.3em)')};
     transition: transform 0.4s ease;
-    box-shadow: ${props => (props.active ? '0 0 10px 1px grey' : '#000000')};
+    letter-spacing: ${props => (props.title === 'all' ? '4px' : 'none')};
+    text-transform: uppercase;
+    font-size: 0.8rem;
   }
 
   &:hover {
