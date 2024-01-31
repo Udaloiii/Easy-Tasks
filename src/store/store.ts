6242bda1005d71f479c0f7d4e752a7314c759d9f
Udaloiii <udaloiiisohappy@gmail.com>
2024-01-31 20:07:55 +0300

    added appReducer in rootReducer; refactored

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,21 @@
+import { useDispatch } from 'react-redux'
+
+import { appReducer } from '@/store/reducers/app-reducer'
 import { taskReducer } from '@/store/reducers/task-reducer'
 import { todoReducer } from '@/store/reducers/todo-reducer'
-import { combineReducers, legacy_createStore as createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { AnyAction, combineReducers } from 'redux'
+import { ThunkDispatch } from 'redux-thunk'
 
 const rootReducer = combineReducers({
+  app: appReducer,
   task: taskReducer,
   todolist: todoReducer,
 })
 
-export const store = createStore(rootReducer)
-export type AppMainType = ReturnType<typeof rootReducer>
+export const store = configureStore({
+  reducer: rootReducer,
+})
+export type AppMainType = ReturnType<typeof store.getState>
+type AppDispatchType = ThunkDispatch<AppMainType, any, AnyAction>
+export const useAppDispatch = () => useDispatch<AppDispatchType>()
