ce31e4a7fbf1c901a4e430d037ed1f35ea00532d
Udaloiii <udaloiiisohappy@gmail.com>
2024-01-31 20:05:05 +0300

    added property "info" in app-reducer.ts; added setAppInfoAC

--- src/store/reducers/app-reducer.ts
+++ src/store/reducers/app-reducer.ts
@@ -1,17 +1,20 @@
 export type AppStatusType = 'failed' | 'idle' | 'loading' | 'succeeded'
 type AppStateType = {
   error: null | string
+  info: null | string // для Snackbar
   isLoading: boolean
   status: AppStatusType
 }
 const initialState: AppStateType = {
   error: null,
+  info: null,
   isLoading: false,
   status: 'loading',
 }
 
 type ActionType =
   | ReturnType<typeof setAppErrorAC>
+  | ReturnType<typeof setAppInfoAC>
   | ReturnType<typeof setAppLoadingAC>
   | ReturnType<typeof setAppStatusAC>
 export const appReducer = (state = initialState, action: ActionType): AppStateType => {
@@ -23,6 +26,9 @@ export const appReducer = (state = initialState, action: ActionType): AppStateTy
 
     case 'SET-APP-ERROR':
       return { ...state, error: action.error }
+
+    case 'SET-APP-INFO':
+      return { ...state, info: action.info }
 
     default:
       return state
@@ -39,4 +45,8 @@ export const setAppLoadingAC = (isLoading: boolean) => {
 
 export const setAppStatusAC = (status: AppStatusType) => {
   return { status, type: 'SET-APP-STATUS' } as const
+}
+
+export const setAppInfoAC = (info: null | string) => {
+  return { info, type: 'SET-APP-INFO' } as const
 }
