ad5e7040906f184ceff9ae00ecd30e0285734ff8
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-02 13:43:28 +0300

    added error & info for messages; added logic for visible Snackbar.tsx; changed style

--- src/components/snackbar/Snackbar.tsx
+++ src/components/snackbar/Snackbar.tsx
@@ -1,14 +1,17 @@
-import { FC } from 'react'
+import { FC, memo, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
+import { setAppErrorAC, setAppInfoAC } from '@/store/reducers/app-reducer'
+import { AppMainType, useAppDispatch } from '@/store/store'
 import { AnimatePresence, motion } from 'framer-motion'
 // eslint-disable-next-line import/no-named-as-default
 import styled from 'styled-components'
 
-type SnackbarPropsType = {
-  sendSuccess?: boolean
-  text?: string
-}
-export const Snackbar: FC<SnackbarPropsType> = ({ sendSuccess, text }: SnackbarPropsType) => {
+export const Snackbar: FC = memo(() => {
+  const error = useSelector<AppMainType, null | string>(state => state.app.error)
+  const info = useSelector<AppMainType, null | string>(state => state.app.info)
+  const dispatch = useAppDispatch()
+
   const variants = {
     exit: { opacity: 0, y: 100 },
     hidden: { opacity: 0, y: 100 },
@@ -20,32 +23,44 @@ export const Snackbar: FC<SnackbarPropsType> = ({ sendSuccess, text }: SnackbarP
     y: { stiffness: 1000, velocity: -100 },
   }
 
+  const conditionVisible = error !== null || info !== null
+
+  useEffect(() => {
+    if (conditionVisible) {
+      setTimeout(() => {
+        dispatch(setAppInfoAC(null))
+        dispatch(setAppErrorAC(null))
+      }, 4000)
+    }
+  }, [error, info, dispatch, conditionVisible])
+
   return (
     <AnimatePresence>
-      {sendSuccess && (
+      {(error || info) && (
         <SendInfo
-          animate={sendSuccess ? 'visible' : 'exit'}
+          animate={conditionVisible ? 'visible' : 'exit'}
+          error={error}
           exit={'exit'}
           initial={'hidden'}
           transition={transition}
           variants={variants}
         >
-          {text}
+          {error ? error : info}
         </SendInfo>
       )}
     </AnimatePresence>
   )
-}
+})
 
-const SendInfo = styled(motion.div)`
+const SendInfo = styled(motion.div)<{ error: null | string }>`
   position: fixed;
   bottom: 80px;
   left: -50%;
   right: -50%;
   width: max-content;
-  border: 1px solid royalblue;
+  border: ${props => (props.error ? '1px solid rgba(139, 0, 0, 0.9)' : '1px solid royalblue')};
   background-color: rgba(24, 24, 24, 0.95);
-  color: rgb(199, 199, 199);
+  color: ${props => (props.error ? 'darkred' : 'whitesmoke')};
   border-radius: 6px;
   padding: 20px;
   margin: 0 auto;
