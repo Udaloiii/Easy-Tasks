01e46ca1b054de90a638606b9b2d2b6268928f76
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-15 20:56:53 +0300

    added isInitialized for initialize app; added Loader if !isInitialized; added routing & routes; added useEffect for authMe

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,44 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { AddItemForm } from '@/components/addItemForm/AddItemForm'
 import { Loader } from '@/components/loader/Loader'
-import { Todolist } from '@/layout/todolist/Todolist'
+import { Snackbar } from '@/components/snackbar/Snackbar'
+import { Login } from '@/layout/login/Login'
+import { Page404 } from '@/layout/pageNotFound/Page404'
+import { TodolistsBox } from '@/layout/todolist/TodolistsBox'
 import { AppStatusType } from '@/store/reducers/app-reducer'
-import { TodolistType, addTodoTC, setTodoTC } from '@/store/reducers/todo-reducer'
+import { authMeTC } from '@/store/reducers/auth-reducer'
 import { AppMainType, useAppDispatch } from '@/store/store'
 import { AnimatePresence } from 'framer-motion'
-// eslint-disable-next-line import/no-named-as-default
-import styled from 'styled-components'
 
 export function App() {
-  const todos = useSelector<AppMainType, TodolistType[]>(state => state.todolist)
+  const isInitialized = useSelector<AppMainType, boolean>(state => state.app.isInitialized)
   const appStatus = useSelector<AppMainType, AppStatusType>(state => state.app.status)
   const dispatch = useAppDispatch()
 
-  const addTodolist = (title: string) => dispatch(addTodoTC(title))
-
   useEffect(() => {
-    dispatch(setTodoTC())
+    dispatch(authMeTC())
   }, [dispatch])
+
+  if (!isInitialized) {
+    return (
+      <AnimatePresence>
+        <Loader />
+      </AnimatePresence>
+    )
+  }
 
   return (
     <div>
-      <FormWrap>
-        <AddItemForm
-          onClick={addTodolist}
-          placeholder={'create you todolist'}
-          textForSnackbar={'todo is added'}
-        />
-      </FormWrap>
       {<AnimatePresence>{appStatus === 'loading' && <Loader />}</AnimatePresence>}
-      <TodoWrap>
-        {todos.map(el => (
-          <Todolist filter={el.filter} id={el.id} key={el.id} title={el.title} />
-        ))}
-      </TodoWrap>
+      <Snackbar />
+      <Routes>
+        <Route element={<TodolistsBox />} path={'/'} />
+        <Route element={<Login />} path={'/login'} />
+        <Route element={<Page404 />} path={'/404'} />
+        <Route element={<Navigate to={'/404'} />} path={'*'} />
+      </Routes>
     </div>
   )
 }
-
-const FormWrap = styled.div`
-  padding: 30px 0 100px;
-
-  input {
-    min-width: 500px;
-    font-size: 1rem;
-  }
-`
-const TodoWrap = styled.div`
-  margin: 0 auto;
-  max-width: 1350px;
-  display: flex;
-  gap: 60px;
-  flex-wrap: wrap;
-  padding: 0 0 50px;
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-`
