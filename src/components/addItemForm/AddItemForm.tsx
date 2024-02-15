fb90a518c63bb28bbac6b0477007bc5c6f75464c
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-15 20:53:47 +0300

    wrap in memo AddItemForm & addItemHandler in useCallback for optimization rerender

--- src/components/addItemForm/AddItemForm.tsx
+++ src/components/addItemForm/AddItemForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, memo, useCallback, useState } from 'react'
 
 import { CustomButton } from '@/components/customButton/CustomButton'
 import { CustomInput } from '@/components/customInput/CustomInput'
@@ -11,49 +11,46 @@ type AddItemFormPropsType = {
   placeholder?: string
   width?: string
 }
-export const AddItemForm: FC<AddItemFormPropsType> = ({
-  onClick,
-  padding,
-  placeholder,
-  width,
-}: AddItemFormPropsType) => {
-  const [error, setError] = useState<null | string>(null)
-  const [value, setValue] = useState('')
-  const addItemHandler = () => {
-    setError(null)
-    if (value.trim() !== '') {
-      onClick?.(value)
-      setValue('')
-    } else {
-      setError('Title is required')
-      const clearId = setTimeout(() => setError(null), 5000)
+export const AddItemForm: FC<AddItemFormPropsType> = memo(
+  ({ onClick, padding, placeholder, width }: AddItemFormPropsType) => {
+    const [error, setError] = useState<null | string>(null)
+    const [value, setValue] = useState('')
+    const addItemHandler = useCallback(() => {
+      error && setError(null)
+      if (value.trim() !== '') {
+        onClick?.(value)
+        setValue('')
+      } else {
+        setError('Title is required')
+        const clearId = setTimeout(() => setError(null), 5000)
 
-      return () => clearInterval(clearId)
-    }
+        return () => clearInterval(clearId)
+      }
+    }, [error, onClick, value])
+
+    return (
+      <Wrap width={width}>
+        <CustomInput
+          error={error}
+          onEnterPress={addItemHandler}
+          onchange={setValue}
+          padding={padding}
+          placeholder={placeholder}
+          setError={setError}
+          value={value}
+        />
+        <CustomButton
+          color={'royalblue'}
+          heightIcon={'35px'}
+          iconId={'add'}
+          onClick={addItemHandler}
+          viewBoxForIcon={'0 0 20 20'}
+          widthIcon={'35px'}
+        />
+      </Wrap>
+    )
   }
-
-  return (
-    <Wrap width={width}>
-      <CustomInput
-        error={error}
-        onEnterPress={addItemHandler}
-        onchange={setValue}
-        padding={padding}
-        placeholder={placeholder}
-        setError={setError}
-        value={value}
-      />
-      <CustomButton
-        color={'royalblue'}
-        heightIcon={'35px'}
-        iconId={'add'}
-        onClick={addItemHandler}
-        viewBoxForIcon={'0 0 20 20'}
-        widthIcon={'35px'}
-      />
-    </Wrap>
-  )
-}
+)
 
 const Wrap = styled.div<{ width?: string }>`
   width: ${props => props.width || '100%'};
