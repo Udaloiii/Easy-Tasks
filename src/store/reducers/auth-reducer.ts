7a93add4a592a4aa8437c91cee110fdc25536faa
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-18 16:22:48 +0300

    moved in redux-toolkit; refactored

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -1,18 +1,7 @@
 import { RequestLogInType, authApi } from '@/api/auth-api'
-import { setAppErrorAC, setAppInitializedAC, setAppStatusAC } from '@/store/reducers/app-reducer'
+import { appActions } from '@/store/reducers/app-reducer'
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { Dispatch } from 'redux'
-
-// type AuthStateType = {
-//   isLogin: boolean
-//   userName: string
-// }
-// const initialState: AuthStateType = {
-//   isLogin: false,
-//   userName: '',
-// }
-
-// type ActionType = ReturnType<typeof setIsLoginAC> | ReturnType<typeof setUserNameAC>
 
 const slice = createSlice({
   initialState: {
@@ -33,86 +22,64 @@ const slice = createSlice({
 export const authReducer = slice.reducer
 export const authActions = slice.actions // экспорт сразу 2 экшенов
 
-// export const authReducer = (state = initialState, action: ActionType): AuthStateType => {
-//   switch (action.type) {
-//     case 'SET-IS-LOGIN':
-//       return { ...state, isLogin: action.isLogin }
-//
-//     case 'SET-USER-NAME':
-//       return { ...state, userName: action.name }
-//     default:
-//       return state
-//   }
-// }
-
-export const setIsLoginAC = (isLogin: boolean) => {
-  return { isLogin, type: 'SET-IS-LOGIN' } as const
-}
-export const setUserNameAC = (name: string) => {
-  return { name, type: 'SET-USER-NAME' } as const
-}
-
 // Thunks
 export const authMeTC = () => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC('loading'))
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
   authApi
     .authMe()
     .then(res => {
       if (res.data.resultCode === 0) {
-        dispatch(setUserNameAC(res.data.data.login))
-        // dispatch(setIsLoginAC(true))
+        dispatch(authActions.setUserName({ name: res.data.data.login }))
         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-        dispatch(setAppInitializedAC(true))
-        dispatch(setAppStatusAC('succeeded'))
+        dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
       } else {
-        dispatch(setAppInitializedAC(true))
-        dispatch(setAppErrorAC(res.data.messages[0]))
-        // dispatch(setIsLoginAC(false))
+        dispatch(appActions.setAppStatus({ status: 'failed' }))
+        dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
       }
     })
     .catch(err => {
-      dispatch(setAppStatusAC('failed'))
-      dispatch(setAppErrorAC(err.message))
+      dispatch(appActions.setAppStatus({ status: 'failed' }))
+      dispatch(appActions.setAppError({ error: err.message }))
     })
 }
 export const loginTC = (form: RequestLogInType) => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC('loading'))
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
   authApi
     .logIn(form)
     .then(res => {
       if (res.data.resultCode === 0) {
-        // dispatch(setIsLoginAC(true))
         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-        dispatch(setAppStatusAC('succeeded'))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
       } else {
-        dispatch(setAppErrorAC(res.data.messages[0]))
-        // dispatch(setIsLoginAC(false))
+        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+        dispatch(appActions.setAppStatus({ status: 'failed' }))
       }
     })
     .catch(err => {
-      dispatch(setAppStatusAC('failed'))
-      dispatch(setAppErrorAC(err.message))
+      dispatch(appActions.setAppStatus({ status: 'failed' }))
+      dispatch(appActions.setAppError({ error: err.message }))
     })
 }
 
 export const logOutTC = () => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC('loading'))
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
   authApi
     .logOut()
     .then(res => {
       if (res.data.resultCode === 0) {
-        dispatch(setIsLoginAC(false))
-        dispatch(setAppStatusAC('succeeded'))
-        // dispatch(setUserNameAC(''))
-        // dispatch(setTodosAC([]))
+        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
       } else {
-        dispatch(setAppErrorAC(res.data.messages[0]))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
       }
     })
     .catch(err => {
-      dispatch(setAppStatusAC('failed'))
-      dispatch(setAppErrorAC(err.message))
+      dispatch(appActions.setAppStatus({ status: 'failed' }))
+      dispatch(appActions.setAppError({ error: err.message }))
     })
 }
