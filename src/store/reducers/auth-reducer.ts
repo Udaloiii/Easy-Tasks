6e887590f885e34be2e0e91bee46d37a72e80bde
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-22 14:02:15 +0300

    moved thunks in createAsyncThunk; refactored in async/await

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -1,7 +1,6 @@
 import { RequestLogInType, authApi } from '@/api/auth-api'
 import { appActions } from '@/store/reducers/app-reducer'
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
-import { Dispatch } from 'redux'
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const slice = createSlice({
   initialState: {
@@ -23,63 +22,117 @@ export const authReducer = slice.reducer
 export const authActions = slice.actions // экспорт сразу 2 экшенов
 
 // Thunks
-export const authMeTC = () => (dispatch: Dispatch) => {
-  dispatch(appActions.setAppStatus({ status: 'loading' }))
-  authApi
-    .authMe()
-    .then(res => {
-      if (res.data.resultCode === 0) {
-        dispatch(authActions.setUserName({ name: res.data.data.login }))
-        dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-        dispatch(appActions.setAppInitialized({ isInitialized: true }))
-        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-      } else {
-        dispatch(appActions.setAppStatus({ status: 'failed' }))
-        dispatch(appActions.setAppInitialized({ isInitialized: true }))
-        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-      }
-    })
-    .catch(err => {
-      dispatch(appActions.setAppStatus({ status: 'failed' }))
-      dispatch(appActions.setAppError({ error: err.message }))
-    })
-}
-export const loginTC = (form: RequestLogInType) => (dispatch: Dispatch) => {
-  dispatch(appActions.setAppStatus({ status: 'loading' }))
-  authApi
-    .logIn(form)
-    .then(res => {
-      if (res.data.resultCode === 0) {
-        dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-      } else {
-        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-        dispatch(appActions.setAppStatus({ status: 'failed' }))
-      }
-    })
-    .catch(err => {
-      dispatch(appActions.setAppStatus({ status: 'failed' }))
-      dispatch(appActions.setAppError({ error: err.message }))
-    })
-}
 
-export const logOutTC = () => (dispatch: Dispatch) => {
+export const authMeTC = createAsyncThunk('auth/authMe', async (_arg, thunkAPI) => {
+  const { dispatch } = thunkAPI
+
   dispatch(appActions.setAppStatus({ status: 'loading' }))
-  authApi
-    .logOut()
-    .then(res => {
-      if (res.data.resultCode === 0) {
-        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-      } else {
-        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-        dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-      }
-    })
-    .catch(err => {
-      dispatch(appActions.setAppStatus({ status: 'failed' }))
-      dispatch(appActions.setAppError({ error: err.message }))
-    })
-}
+  const res = await authApi.authMe()
+
+  if (res.data.resultCode === 0) {
+    dispatch(authActions.setUserName({ name: res.data.data.login }))
+    dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+    dispatch(appActions.setAppInitialized({ isInitialized: true }))
+    dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+  } else {
+    dispatch(appActions.setAppStatus({ status: 'failed' }))
+    dispatch(appActions.setAppInitialized({ isInitialized: true }))
+    dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+    dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+  }
+})
+export const loginTC = createAsyncThunk('auth/login', async (form: RequestLogInType, thunkAPI) => {
+  const { dispatch } = thunkAPI
+
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
+  const res = await authApi.logIn(form)
+
+  if (res.data.resultCode === 0) {
+    dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+    dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+  } else {
+    dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+    dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+    dispatch(appActions.setAppStatus({ status: 'failed' }))
+  }
+})
+export const logOutTC = createAsyncThunk('auth/logout', async (_arg, thunkAPI) => {
+  const { dispatch } = thunkAPI
+
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
+  const res = await authApi.logOut()
+
+  try {
+    if (res.data.resultCode === 0) {
+      dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+      dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+    } else {
+      dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+      dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+    }
+  } catch (err) {
+    dispatch(appActions.setAppStatus({ status: 'failed' }))
+    dispatch(appActions.setAppError({ error: (err as Error).message })) // типизировал
+  }
+})
+
+// export const _authMeTC = () => (dispatch: Dispatch) => {
+//   dispatch(appActions.setAppStatus({ status: 'loading' }))
+//   authApi
+//     .authMe()
+//     .then(res => {
+//       if (res.data.resultCode === 0) {
+//         dispatch(authActions.setUserName({ name: res.data.data.login }))
+//         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+//         dispatch(appActions.setAppInitialized({ isInitialized: true }))
+//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+//       } else {
+//         dispatch(appActions.setAppStatus({ status: 'failed' }))
+//         dispatch(appActions.setAppInitialized({ isInitialized: true }))
+//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+//       }
+//     })
+//     .catch(err => {
+//       dispatch(appActions.setAppStatus({ status: 'failed' }))
+//       dispatch(appActions.setAppError({ error: err.message }))
+//     })
+// }
+// export const _loginTC = (form: RequestLogInType) => (dispatch: Dispatch) => {
+//   dispatch(appActions.setAppStatus({ status: 'loading' }))
+//   authApi
+//     .logIn(form)
+//     .then(res => {
+//       if (res.data.resultCode === 0) {
+//         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+//       } else {
+//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+//         dispatch(appActions.setAppStatus({ status: 'failed' }))
+//       }
+//     })
+//     .catch(err => {
+//       dispatch(appActions.setAppStatus({ status: 'failed' }))
+//       dispatch(appActions.setAppError({ error: err.message }))
+//     })
+// }
+
+// export const _logOutTC = () => (dispatch: Dispatch) => {
+//   dispatch(appActions.setAppStatus({ status: 'loading' }))
+//   authApi
+//     .logOut()
+//     .then(res => {
+//       if (res.data.resultCode === 0) {
+//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+//       } else {
+//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+//       }
+//     })
+//     .catch(err => {
+//       dispatch(appActions.setAppStatus({ status: 'failed' }))
+//       dispatch(appActions.setAppError({ error: err.message }))
+//     })
+// }
