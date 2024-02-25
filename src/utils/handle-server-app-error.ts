dd0d50ceb84b49f2e6d0305858bbede4f7e6e0fb
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-25 20:23:37 +0300

    moved function for handling errors in separate files

---
+++ src/utils/handle-server-app-error.ts
@@ -0,0 +1,12 @@
+import { MainResponseType } from '@/api/main-instance-api'
+import { appActions } from '@/store/reducers/app-reducer'
+import { Dispatch } from 'redux'
+
+export const handleServerAppError = <D>(data: MainResponseType<D>, dispatch: Dispatch) => {
+  if (data.messages.length) {
+    dispatch(appActions.setAppError({ error: data.messages[0] }))
+  } else {
+    dispatch(appActions.setAppError({ error: 'Some error' }))
+  }
+  dispatch(appActions.setAppStatus({ status: 'failed' }))
+}
--- src/utils/handle-server-network-error.ts
+++ src/utils/handle-server-network-error.ts
@@ -1,16 +1,6 @@
-import { MainResponseType } from '@/api/main-instance-api'
 import { appActions } from '@/store/reducers/app-reducer'
 import axios, { AxiosError } from 'axios'
 import { Dispatch } from 'redux'
-
-export const handleServerAppError = <D>(data: MainResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }))
-  } else {
-    dispatch(appActions.setAppError({ error: 'Some error' }))
-  }
-  dispatch(appActions.setAppStatus({ status: 'failed' }))
-}
 
 export const handleServerNetworkError = (e: unknown, dispatch: Dispatch) => {
   const err = e as AxiosError<{ error: string }> | Error
