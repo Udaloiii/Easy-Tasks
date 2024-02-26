f7ad62173df638f2629414efb2b4185122de66a6
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-26 17:08:31 +0300

    moved thunks in createAsyncThunk; refactored

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -1,8 +1,17 @@
 import { RequestLogInType, authApi } from '@/api/auth-api'
+import { ResultCode } from '@/api/main-instance-api'
 import { appActions } from '@/store/reducers/app-reducer'
+import { createAppAsyncThunk } from '@/utils/create-app-async-thunk'
+import { handleServerAppError } from '@/utils/handle-server-app-error'
+import { handleServerNetworkError } from '@/utils/handle-server-network-error'
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const slice = createSlice({
+  extraReducers: builder => {
+    builder.addCase(authThunks.authMe.fulfilled, (state, action) => {
+      state.isLogin = action.payload.isLoggedIn
+    })
+  },
   initialState: {
     isLogin: false,
     userName: '',
@@ -23,31 +32,44 @@ export const authActions = slice.actions // экспорт сразу 2 экше
 
 // Thunks
 
-const authMe = createAsyncThunk('auth/authMe', async (_arg, thunkAPI) => {
-  const { dispatch } = thunkAPI
+const authMe = createAppAsyncThunk<{ isLoggedIn: boolean }>(
+  'auth/authMe',
+  async (_arg, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI
 
-  dispatch(appActions.setAppStatus({ status: 'loading' }))
-  const res = await authApi.authMe()
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
+    const res = await authApi.authMe()
 
-  if (res.data.resultCode === 0) {
-    dispatch(authActions.setUserName({ name: res.data.data.login }))
-    dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-    dispatch(appActions.setAppInitialized({ isInitialized: true }))
-    dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-  } else {
-    dispatch(appActions.setAppStatus({ status: 'failed' }))
-    dispatch(appActions.setAppInitialized({ isInitialized: true }))
-    dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-    dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+    try {
+      if (res.data.resultCode === ResultCode.Success) {
+        dispatch(authActions.setUserName({ name: res.data.data.login }))
+        // dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+        dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+
+        return { isLoggedIn: true }
+      } else {
+        dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+
+        handleServerAppError(res.data, dispatch)
+
+        return rejectWithValue(null)
+      }
+    } catch (err) {
+      handleServerNetworkError(err, dispatch)
+
+      return rejectWithValue(null)
+    }
   }
-})
+)
 const logIn = createAsyncThunk('auth/login', async (form: RequestLogInType, thunkAPI) => {
   const { dispatch } = thunkAPI
 
   dispatch(appActions.setAppStatus({ status: 'loading' }))
   const res = await authApi.logIn(form)
 
-  if (res.data.resultCode === 0) {
+  if (res.data.resultCode === ResultCode.Success) {
     dispatch(authActions.setIsLogin({ isLoggedIn: true }))
     dispatch(appActions.setAppStatus({ status: 'succeeded' }))
   } else {
@@ -63,7 +85,7 @@ const logOut = createAsyncThunk('auth/logout', async (_arg, thunkAPI) => {
   const res = await authApi.logOut()
 
   try {
-    if (res.data.resultCode === 0) {
+    if (res.data.resultCode === ResultCode.Success) {
       dispatch(authActions.setIsLogin({ isLoggedIn: false }))
       dispatch(appActions.setAppStatus({ status: 'succeeded' }))
       dispatch(authActions.setUserName({ name: '' }))
@@ -78,63 +100,3 @@ const logOut = createAsyncThunk('auth/logout', async (_arg, thunkAPI) => {
 })
 
 export const authThunks = { authMe, logIn, logOut }
-// export const _authMeTC = () => (dispatch: Dispatch) => {
-//   dispatch(appActions.setAppStatus({ status: 'loading' }))
-//   authApi
-//     .authMe()
-//     .then(res => {
-//       if (res.data.resultCode === 0) {
-//         dispatch(authActions.setUserName({ name: res.data.data.login }))
-//         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-//         dispatch(appActions.setAppInitialized({ isInitialized: true }))
-//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-//       } else {
-//         dispatch(appActions.setAppStatus({ status: 'failed' }))
-//         dispatch(appActions.setAppInitialized({ isInitialized: true }))
-//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-//       }
-//     })
-//     .catch(err => {
-//       dispatch(appActions.setAppStatus({ status: 'failed' }))
-//       dispatch(appActions.setAppError({ error: err.message }))
-//     })
-// }
-// export const _loginTC = (form: RequestLogInType) => (dispatch: Dispatch) => {
-//   dispatch(appActions.setAppStatus({ status: 'loading' }))
-//   authApi
-//     .logIn(form)
-//     .then(res => {
-//       if (res.data.resultCode === 0) {
-//         dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-//       } else {
-//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-//         dispatch(appActions.setAppStatus({ status: 'failed' }))
-//       }
-//     })
-//     .catch(err => {
-//       dispatch(appActions.setAppStatus({ status: 'failed' }))
-//       dispatch(appActions.setAppError({ error: err.message }))
-//     })
-// }
-
-// export const _logOutTC = () => (dispatch: Dispatch) => {
-//   dispatch(appActions.setAppStatus({ status: 'loading' }))
-//   authApi
-//     .logOut()
-//     .then(res => {
-//       if (res.data.resultCode === 0) {
-//         dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-//       } else {
-//         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-//         dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-//       }
-//     })
-//     .catch(err => {
-//       dispatch(appActions.setAppStatus({ status: 'failed' }))
-//       dispatch(appActions.setAppError({ error: err.message }))
-//     })
-// }
