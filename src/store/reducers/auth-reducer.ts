31ad5200878274f12f605999268067d88e47f36c
Udaloiii <udaloiiisohappy@gmail.com>
2024-02-27 18:45:44 +0300

    moved thunks in createAsyncThunk; refactored

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -4,13 +4,20 @@ import { appActions } from '@/store/reducers/app-reducer'
 import { createAppAsyncThunk } from '@/utils/create-app-async-thunk'
 import { handleServerAppError } from '@/utils/handle-server-app-error'
 import { handleServerNetworkError } from '@/utils/handle-server-network-error'
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 const slice = createSlice({
   extraReducers: builder => {
-    builder.addCase(authThunks.authMe.fulfilled, (state, action) => {
-      state.isLogin = action.payload.isLoggedIn
-    })
+    builder
+      .addCase(authThunks.authMe.fulfilled, (state, action) => {
+        state.isLogin = action.payload.isLoggedIn
+      })
+      .addCase(authThunks.logIn.fulfilled, (state, action) => {
+        state.isLogin = action.payload.isLoggedIn
+      })
+      .addCase(authThunks.logOut.fulfilled, (state, action) => {
+        state.isLogin = action.payload.isLoggedIn
+      })
   },
   initialState: {
     isLogin: false,
@@ -38,20 +45,50 @@ const authMe = createAppAsyncThunk<{ isLoggedIn: boolean }>(
     const { dispatch, rejectWithValue } = thunkAPI
 
     dispatch(appActions.setAppStatus({ status: 'loading' }))
-    const res = await authApi.authMe()
 
     try {
+      const res = await authApi.authMe() // если вне блока try, то не обрабатывается ошибка и крутилка бесконечно
+
       if (res.data.resultCode === ResultCode.Success) {
         dispatch(authActions.setUserName({ name: res.data.data.login }))
         // dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-        dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        // dispatch(appActions.setAppInitialized({ isInitialized: true }))
         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
 
         return { isLoggedIn: true }
       } else {
-        dispatch(appActions.setAppInitialized({ isInitialized: true }))
-        dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+        // dispatch(appActions.setAppInitialized({ isInitialized: true }))
+        // dispatch(authActions.setIsLogin({ isLoggedIn: false }))
 
+        handleServerAppError(res.data, dispatch)
+
+        return rejectWithValue(null)
+      }
+    } catch (err) {
+      handleServerNetworkError(err, dispatch)
+
+      return rejectWithValue(null)
+    } finally {
+      dispatch(appActions.setAppInitialized({ isInitialized: true }))
+    }
+  }
+)
+const logIn = createAppAsyncThunk<{ isLoggedIn: true }, RequestLogInType>(
+  'auth/login',
+  async (form, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI
+
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
+    try {
+      const res = await authApi.logIn(form) // если вне блока try, то не обрабатывается ошибка и крутилка бесконечно
+
+      if (res.data.resultCode === ResultCode.Success) {
+        // dispatch(authActions.setIsLogin({ isLoggedIn: true }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+
+        return { isLoggedIn: true }
+      } else {
+        // dispatch(authActions.setIsLogin({ isLoggedIn: false }))
         handleServerAppError(res.data, dispatch)
 
         return rejectWithValue(null)
@@ -63,40 +100,37 @@ const authMe = createAppAsyncThunk<{ isLoggedIn: boolean }>(
     }
   }
 )
-const logIn = createAsyncThunk('auth/login', async (form: RequestLogInType, thunkAPI) => {
-  const { dispatch } = thunkAPI
+const logOut = createAppAsyncThunk<{ isLoggedIn: boolean }>(
+  'auth/logout',
+  async (_arg, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI
 
-  dispatch(appActions.setAppStatus({ status: 'loading' }))
-  const res = await authApi.logIn(form)
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
 
-  if (res.data.resultCode === ResultCode.Success) {
-    dispatch(authActions.setIsLogin({ isLoggedIn: true }))
-    dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-  } else {
-    dispatch(appActions.setAppError({ error: res.data.messages[0] }))
-    dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-    dispatch(appActions.setAppStatus({ status: 'failed' }))
-  }
-})
-const logOut = createAsyncThunk('auth/logout', async (_arg, thunkAPI) => {
-  const { dispatch } = thunkAPI
+    try {
+      const res = await authApi.logOut() // если вне блока try, то не обрабатывается ошибка и крутилка бесконечно
 
-  dispatch(appActions.setAppStatus({ status: 'loading' }))
-  const res = await authApi.logOut()
+      if (res.data.resultCode === ResultCode.Success) {
+        // dispatch(authActions.setIsLogin({ isLoggedIn: false }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+        dispatch(authActions.setUserName({ name: '' }))
 
-  try {
-    if (res.data.resultCode === ResultCode.Success) {
-      dispatch(authActions.setIsLogin({ isLoggedIn: false }))
-      dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-      dispatch(authActions.setUserName({ name: '' }))
-    } else {
-      dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-      dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+        return { isLoggedIn: false }
+      } else {
+        // dispatch(appActions.setAppStatus({ status: 'failed' }))
+        // dispatch(appActions.setAppError({ error: res.data.messages[0] }))
+        handleServerAppError(res.data, dispatch)
+
+        return rejectWithValue(null)
+      }
+    } catch (err) {
+      // dispatch(appActions.setAppStatus({ status: 'failed' }))
+      // dispatch(appActions.setAppError({ error: (err as Error).message })) // типизировал
+      handleServerNetworkError(err, dispatch)
+
+      return rejectWithValue(null)
     }
-  } catch (err) {
-    dispatch(appActions.setAppStatus({ status: 'failed' }))
-    dispatch(appActions.setAppError({ error: (err as Error).message })) // типизировал
   }
-})
+)
 
 export const authThunks = { authMe, logIn, logOut }
