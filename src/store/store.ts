ec5adb474b533a8b53589ce843c28d3c0e5f0e26
Udaloiii <udaloiiisohappy@gmail.com>
2024-03-02 12:00:16 +0300

    added reducers in store without combineReducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,20 +5,16 @@ import { authReducer } from '@/store/reducers/auth-reducer'
 import { taskReducer } from '@/store/reducers/task-reducer'
 import { todoReducer } from '@/store/reducers/todo-reducer'
 import { configureStore } from '@reduxjs/toolkit'
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-  app: appReducer,
-  auth: authReducer,
-  task: taskReducer,
-  todolist: todoReducer,
-})
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    app: appReducer,
+    auth: authReducer,
+    task: taskReducer,
+    todolist: todoReducer,
+  },
 })
 export type AppMainType = ReturnType<typeof store.getState>
-// type AppDispatchType = ThunkDispatch<AppMainType, any, AnyAction>
 export type AppDispatchType = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatchType>()
 
